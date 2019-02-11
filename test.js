fed390085c0ce575072a198eba547cfa0a211410
Joydip Roy <joydipand@gmail.com>
2019-02-11 17:37:49 +0530

    seperate lint and test

--- test.js
+++ test.js
@@ -1,29 +1,27 @@
 'use strict'
 
-const t = require('tap')
-const test = t.test
-const Fastify = require('fastify')
-const { join } = require('path')
-const { readFileSync } = require('fs')
+const t = require('tap');
+const test = t.test;
+const Fastify = require('fastify');
 
 test('should return an html document', t => {
-  t.plan(3)
+  t.plan(3);
 
-  const fastify = Fastify()
+  const fastify = Fastify();
   fastify
-    .register(require('./index'))
+    .register(require('fastify-nextjs'))
     .after(() => {
-      fastify.next('/hello')
-    })
+      fastify.next('/')
+    });
 
   fastify.inject({
-    url: '/hello',
+    url: '/',
     method: 'GET'
   }, (err, res) => {
     t.error(err)
     t.equal(res.statusCode, 200)
     t.equal(res.headers['content-type'], 'text/html; charset=utf-8')
-  })
+  });
 
-  fastify.close()
+  fastify.close();
 });
